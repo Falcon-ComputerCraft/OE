e506444f9d169c2c4bd40b4b5722cb2f485104da
Haider <138309150+Haider982@users.noreply.github.com>
2026-03-29 12:47:02 +0400

    Center the boot screen
    My eyes thank me.

--- main.lua
+++ main.lua
@@ -2,5 +2,5 @@ local settings = {
     ["GithubURL"] = "https://github.com/Falcon-ComputerCraft/Falcon-OE"
 }
 
-print("         Falcon OE         ")
-print(settings["GithubURL"])
+print("                   Falcon OE")
+print("| " .. settings["GithubURL"] .. " |")
