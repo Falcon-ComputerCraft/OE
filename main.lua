9c5f537e71e730bcd6d18ca2cd4dbaeda29ff957
Haider <138309150+Haider982@users.noreply.github.com>
2026-03-29 18:40:56 +0400

    Add a scrollDown function
    Adds a scrollDown function and uses it to space the OE name and the GitHub URL.

--- main.lua
+++ main.lua
@@ -2,5 +2,10 @@ local settings = {
     ["GithubURL"] = "https://github.com/Falcon-ComputerCraft/Falcon-OE"
 }
 
+local function scrollDown(y)
+    term.scroll(-y)
+end
+
 print("                   Falcon OE")
+scrollDown(3)
 print(settings["GithubURL"])
