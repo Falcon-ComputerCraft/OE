b56a54fc16f8a76a3bb0e21316a34d35b118d24a
Haider <138309150+Haider982@users.noreply.github.com>
2026-03-31 10:36:59 +0400

    Add unit tessts.
    Add a unitTestsEnabled bool and add the logic for unit tests. Also added visualUnitTests that ask the user to confirm if the unit test was successful. This runs in the ComputerCraft environment making it better then a lua environment in Python.

--- main.lua
+++ main.lua
@@ -1,3 +1,4 @@
+local unitTestsEnabled = false
 local settings = {
     ["GithubURL"] = "https://github.com/Falcon-ComputerCraft/Falcon-OE"
 }
@@ -7,6 +8,21 @@ local settings = {
 local function scrollDown(y)
     term.scroll(-y)
 end
+
+-- UNIT TESTS START
+
+if unitTestsEnabled == true then
+    print("Running unit tests...")
+    print("Running visualUnitTest for scrollDown()")
+    scrollDown(3)
+    print("Confirm if the terminal content has scrolled down by 3 lines.")
+    print("Unit tests completed.")
+    sleep(3)
+    print("Stopping program.")
+    return
+end
+
+-- UNIT TESTS END
 
 print("                   Falcon OE")
 scrollDown(3)
